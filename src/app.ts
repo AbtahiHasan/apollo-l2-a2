f5836ac79985443c2721c874d3dec448d874907c
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-22 21:45:53 +0600

    user router setup done

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application } from 'express';
 import cors from 'cors';
+import { userRouter } from './app/modules/user/user.routes';
 
 const app: Application = express();
 
 // parsers
 app.use(cors());
 app.use(express.json());
+
+// router configuration
+app.use('/api/users', userRouter);
 
 export default app;
