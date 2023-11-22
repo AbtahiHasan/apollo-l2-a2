0fa03f1a6ffda15851f1d7c53f572441367e5da0
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-22 21:34:59 +0600

    orders array of objects added user interface

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,3 +1,9 @@
+export type Order = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
 export type User = {
   userId: number;
   username: string;
@@ -15,4 +21,5 @@ export type User = {
     city: string;
     country: string;
   };
+  orders: Order[];
 };
