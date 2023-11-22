55691647a882e1336a7cc902c5c61f3d15dd034c
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-22 21:42:02 +0600

    user model done

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,72 @@
+import mongoose, { Model, Schema } from 'mongoose';
+import { Order, User } from './user.interface';
+
+const orderSchema = new Schema<Order>({
+  productName: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+const userSchema = new Schema<User>({
+  userId: {
+    type: Number,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    require: true,
+  },
+  hobbies: [String],
+  address: {
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  orders: [orderSchema],
+});
+
+const UserModel: Model<User> = mongoose.model('user', userSchema);
+export default UserModel;
