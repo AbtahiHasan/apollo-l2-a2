132dc037c89f22e296574000d5001435749386b4
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-23 15:00:04 +0600

    get all  users done

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -26,9 +26,32 @@ const createUser = async (req: Request, res: Response) => {
     });
   }
 };
+const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const result = await userServices.getAllUserIntoDb();
+
+    res.status(200).json({
+      success: true,
+      message: 'Users fetched successfully!',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      message: error.message || 'internal server error',
+      error: error,
+      // error: {
+      //   code: 400,
+      //   description: 'User not found!',
+      // },
+    });
+  }
+};
 
 const userController = {
   createUser,
+  getAllUsers,
 };
 
 export default userController;
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -5,9 +5,20 @@ const createUserIntoDb = async (user: User) => {
   const result = await UserModel.create(user);
   return result;
 };
+const getAllUserIntoDb = async () => {
+  const result = await UserModel.find().select({
+    username: 1,
+    fullName: 1,
+    age: 1,
+    email: 1,
+    address: 1,
+  });
+  return result;
+};
 
 const userServices = {
   createUserIntoDb,
+  getAllUserIntoDb,
 };
 
 export default userServices;
