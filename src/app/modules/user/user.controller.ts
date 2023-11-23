d334054dfcd08348eeeea75c4b518c4ecd1dd5e7
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-23 14:50:55 +0600

    zod validation added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,23 +1,28 @@
 import { Request, Response } from 'express';
 import userServices from './user.service';
+import userValidationSchema from './user.validation';
 const createUser = async (req: Request, res: Response) => {
   try {
     const userData = req.body;
+    const zodParsedData = userValidationSchema.parse(userData);
 
-    const result = await userServices.createUserIntoDb(userData);
+    const result = await userServices.createUserIntoDb(zodParsedData);
+
     res.status(200).json({
       success: true,
       message: 'user created successfully!',
       data: result,
     });
-  } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
     return res.status(500).json({
       success: false,
-      message: 'User not found',
-      error: {
-        code: 404,
-        description: 'User not found!',
-      },
+      message: error.message || 'internal server error',
+      error: error,
+      // error: {
+      //   code: 400,
+      //   description: 'User not found!',
+      // },
     });
   }
 };
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+const orderSchema = z.object({
+  productName: z.string(),
+  price: z.number(),
+  quantity: z.number(),
+});
+
+const userValidationSchema = z.object({
+  userId: z.number(),
+  username: z.string(),
+  password: z.string(),
+  fullName: z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+  }),
+  age: z.number(),
+  email: z.string().email(),
+  isActive: z.boolean(),
+  hobbies: z.string().array(),
+  address: z.object({
+    street: z.string(),
+    city: z.string(),
+    country: z.string(),
+  }),
+  orders: z.array(orderSchema),
+});
+
+export default userValidationSchema;
